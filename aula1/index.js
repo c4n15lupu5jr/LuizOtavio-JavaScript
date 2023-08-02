9be1799dc62dcf0c181e7c12cee1729dd4746d0a
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-08-02 10:10:55 -0300

    Aulas do Luiz Otávio
    Curso da Udemy

---
+++ aula1/index.js
@@ -0,0 +1,5 @@
+console.log(" 'Hello world' "); //String = Texto
+console.log(' "Hello world" '); // Number = Número
+console.log(`Hello World`)
+
+console.log(35, 15.85, 'Hello world');
---
+++ aula2/comentarios.js
@@ -0,0 +1,7 @@
+// E escrever um comentário
+console.log('Hello world'); // Aqui tem outro comentário
+console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+// Vai ser ignorado
