fde04c5a4f8bd72c5a7cd7b1e6802b7c1db97c68
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-08-22 10:32:30 -0300

    Aulas de JS
    Aulas de JavaScript do professor Luiz Otávio da Udemy

---
+++ aula18/index.js
@@ -0,0 +1,9 @@
+function criaPessoa (nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    }
+}
+
+const pessoa1 = criaPessoa('Luiz', 'Otávio', 25)
