1ccf8c20b3c00f6286aba373fd54edce81c05c45
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-08-24 09:39:38 -0300

    Aulas de JS
    Aulas de JavaScript do professor Luiz Otávio da Udemy

---
+++ aula21/index.js
@@ -0,0 +1,14 @@
+/* Operadores de comparação 
+> 
+>=
+<
+<=
+==
+===
+!=
+!==
+*/
+const num1 = 10 
+const num2 = 10 
+const comp = num1 == num2
+console.log(comp)
---
+++ aula22/index.js
@@ -0,0 +1,13 @@
+/* Operadores Lógicos 
+&& -> AND -> E
+|| -> OR -> OU
+! -> NOT -> NÃO
+*/
+// const expressaoAnd = true && true && true && true
+// const expressaoOr = false || false || false || false
+
+// const usuario = 'Luiz'
+// const senha = '123456'
+
+// const vaiLogar = usuario === 'Luiz' && senha === '12345'
+console.log(!false)
