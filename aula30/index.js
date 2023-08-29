bae1477dea69c9edd10956122fde13ebe351412e
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-08-29 10:01:55 -0300

    Aulas de JS
    Aulas de Javascript do professor Luiz Otávio da Udemy

---
+++ aula30/index.js
@@ -0,0 +1,36 @@
+const h1 = document.querySelector('.container h1')
+h1.innerHTML
+
+const data = new Date('')
+const diaSemana = data.getDay()
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado']
+    return diasSemana[diaSemana]
+        }
+
+
+function getNomeMes(numeroMes) {
+    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+        'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+        return meses[numeroMes]
+            }
+
+    function zeroAEsquerda (num) {
+        return num >= 10 ? num : `0${num}`
+    }
+
+    function criaData(data) {
+        const diaSemana = data.getDay()
+        const numeroMes = data.getMonth()
+
+        const nomeDia = getDiaSemanaTexto(diaSemana)
+        const nomeMes = getNomeMes(numeroMes)
+
+        return (
+            `${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+            ` de ${data.getFullYear()} ` +
+            `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+        )
+    }
+
+    h1.innerHTML = criaData(data)
