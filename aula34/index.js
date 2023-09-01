ebaf07b381e8a7e2ff9f20179073993eb2b16bab
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-09-01 09:49:32 -0300

    Aulas de JS
    Aulas de Javascript do professor Luiz Otávio da Udemy

--- aula34/index.js
+++ aula34/index.js
@@ -1,3 +1,3 @@
-for (let i = 0; i <= 5; i++) {
-    console.log(i)
+for (let i = 0; i <= 10; i++) {
+    console.log(`Linha ${i}`)
 }
---
+++ aula36/index.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio',
+    idade: 30
+}
+
+console.log(pessoa.nome)
+console.log(pessoa['nome'])
+
+
+
+// for (let indice in frutas) {
+//    console.log(frutas[indice])
+// }
