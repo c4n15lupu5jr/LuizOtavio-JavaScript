18d90a310564781910eb51645c9daee0e44fc86a
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-09-15 09:36:51 -0300

    Aulas de JS
    Aulas de JavaScript do professor Luiz Otávio da Udemy

---
+++ aula49/index.js
@@ -0,0 +1,12 @@
+// Declaração de função
+falaOi()
+function falaOi() {
+    console.log('Oi')
+}
+
+// First-class objects
+// Function expression
+const souUmDado = function () {
+    console.log('Sou um dado.')
+}
+souUmDado()
