9588a5ad8445fbd169b325f604f00fb91ed81965
c4n15lupu5jr <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-10-10 09:52:38 -0300

    Aulas de JS
    Aulas de JavaScript do professor Luiz Otávio da Udemy

---
+++ aula62/index.js
@@ -0,0 +1,7 @@
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana']
+// const novo = nomes.slice(1, -1)
+// console.log(novo)
+
+const nomes = ['Luiz', 'Otávio', 'Miranda']
+const nome = nomes.join(' ')
+console.log(nomes)
---
+++ aula63/index.js
@@ -0,0 +1,5 @@
+const nomes = ['Maria', 'João', 'Eduardo', 'Gabriel', 'Júlia']
+
+
+const removidos = nomes.splice(3, 2)
+console.log(nomes,removidos)
