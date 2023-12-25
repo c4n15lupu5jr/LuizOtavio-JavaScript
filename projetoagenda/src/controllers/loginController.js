720703efaa455023f11a52e00049a05d1c113158
Renan Lobo <84355075+c4n15lupu5jr@users.noreply.github.com>
2023-12-25 11:02:32 -0300

    Aulas de JS
    Aulas de JavaScript do professor Luiz Otávio da Udemy

--- projetoagenda/src/controllers/loginController.js
+++ projetoagenda/src/controllers/loginController.js
@@ -1,59 +1,30 @@
-const Login = require('../models/LoginModel');
+const login = require('../models/LoginModel')
 
 exports.index = (req, res) => {
-  if(req.session.user) return res.render('login-logado');
-  return res.render('login');
-};
+  res.render('login')
+}
 
 exports.register = async function(req, res) {
   try {
-    const login = new Login(req.body);
-    await login.register();
+    const login = new Login(req.body)
+  await login.register()
 
-    if(login.errors.length > 0) {
-      req.flash('errors', login.errors);
-      req.session.save(function() {
-        return res.redirect('back');
-      });
-      return;
-    }
-
-    req.flash('success', 'Seu usuário foi criado com sucesso.');
+  if(login.errors.length > 0) {
+    req.flash('errors', login.errors)
     req.session.save(function() {
-      return res.redirect('back');
-    });
-  } catch(e) {
-    console.log(e);
-    return res.render('404');
+      res.redirect('back')
+    })
+    return
   }
-};
 
-exports.login = async function(req, res) {
-  try {
-    const login = new Login(req.body);
-    await login.login();
-
-    if(login.errors.length > 0) {
-      req.flash('errors', login.errors);
-      req.session.save(function() {
-        return res.redirect('back');
-      });
-      return;
-    }
-
-    req.flash('success', 'Você entrou no sistema.');
-    req.session.user = login.user;
-    req.session.save(function() {
-      return res.redirect('back');
-    });
+  req.flash('success', 'Seu usuário foi criado com sucesso.')
+  req.session.save(function() {
+    return res.redirect('back')
+  })
   } catch(e) {
-    console.log(e);
-    return res.render('404');
+    console.log(e)
+    return res.render('404')
   }
-};
 
-exports.logout = function(req, res) {
-  req.session.destroy();
-  res.redirect('/');
-};
-
+  
+}
