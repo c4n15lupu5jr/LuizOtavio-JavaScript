b4abe4f3cf590117c6376e1c7bee0a115dfd9b66
Renan Lobo <renan.lobo08@hotmail.com>
2024-05-06 13:06:33 -0300

    Aulas de TS do professor Luiz Otávio da Udemy

---
+++ REACT/TypeScript/dist/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!************************************!*\
+  !*** ./src/A0018-webpack/index.ts ***!
+  \************************************/
+
+console.log('Olá Mundo!');
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
--- REACT/TypeScript/src/A0018-webpack/index.ts
+++ REACT/TypeScript/src/A0018-webpack/index.ts
@@ -1,1 +1,1 @@
-console.log('Olá Mundo!')
+import './mod'
