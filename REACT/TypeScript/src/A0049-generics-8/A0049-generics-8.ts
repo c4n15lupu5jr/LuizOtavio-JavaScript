51840a44e3ad8403b952ed5ec96487016fb49479
Renan Lobo <renan.lobo08@hotmail.com>
2024-06-24 15:17:36 -0300

    Aula de TS do professor Luiz Otávio da Udemy

--- REACT/TypeScript/src/A0049-generics-8/A0049-generics-8.ts
+++ REACT/TypeScript/src/A0049-generics-8/A0049-generics-8.ts
@@ -28,5 +28,37 @@ const objeto2: PessoaProtocol = {
 };
 console.log(objeto2);
 
+// Extract e Exclude
+type ABC = 'A' | 'B' | 'C';
+type CDE = 'C' | 'D' | 'E';
+type TipoExclude = Exclude<ABC, CDE>;
+type TipoExtract = Extract<ABC, CDE>;
+
+//
+type AccountMongo = {
+  _id: string;
+  nome: string;
+  idade: number;
+};
+
+type AccountApi = {
+  _id: string;
+  nome: string;
+  idade: number;
+};
+
+const accountMongo: AccountMongo = {
+  _id: 'abcde12345',
+  nome: 'Luiz',
+  idade: 30,
+};
+
+function mapAccount(accountMongo: AccountMongo): AccountApi {
+  const { _id, ...accountData } = accountMongo;
+  return { ...accountData, id: _id };
+}
+
+const AccountApi = mapAccount(accountMongo)
+
 //Module mode
 export default 1;
